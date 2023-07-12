6c71bb42715c0d9cf9ab9fdf1bddab64186e7723
Kay Cee <se.kaycee@gmail.com>
2023-07-12 21:23:55 +0100

    feat: solve task 4. Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+-- write a SQL script that creates a trigger
+     DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$ 
+   CREATE TRIGGER reduce_quantity
+    AFTER
+   INSERT ON orders FOR EACH ROW BEGIN
+   UPDATE items
+      SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+      END $$ 
+DELIMITER;
