decf4f652aa277f7876640a63ea5ce39a482b093
Kay Cee <se.kaycee@gmail.com>
2023-07-20 01:06:29 +0100

    feat: increment values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 ''' task: Working with Redis '''
+import uuid
+import redis
+from functools import wraps
+from typing import Any, Callable, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    ''' track the number of calls made to a method in a Cache class. '''
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        ''' invoke the given method after incrementing its call counter. '''
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
+
 
 class Cache:
     ''' represent an object for storing data in a Redis data storage. '''
