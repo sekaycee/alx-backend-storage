f8ec0f70be5431d8484bed8c4b21d4903cd33185
Kay Cee <se.kaycee@gmail.com>
2023-07-20 01:02:01 +0100

    feat: read from redis and recover original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,3 +15,20 @@ class Cache:
         data_key = str(uuid.uuid4())
         self._redis.set(data_key, data)
         return data_key
+    
+    def get(
+            self,
+            key: str,
+            fn: Callable = None,
+            ) -> Union[str, bytes, int, float]:
+        ''' retrieve a value from a Redis data storage. '''
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        ''' retrieve a string value from a Redis data storage. '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        ''' retrieve an integer value from a Redis data storage. '''
+        return self.get(key, lambda x: int(x))
